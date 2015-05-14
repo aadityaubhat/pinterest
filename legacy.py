705c272a769879d84b16bb0c2b62b8e03d0ce1be
AadityaBhat <aadityaubhat@gmail.com>
2015-05-14 03:04:23 -0400

    initial commit

---
+++ legacy.py
@@ -0,0 +1,16 @@
+__author__ = 'aaditya'
+import config
+import entity
+
+
+def read_line(line):
+    """
+    Read a single line and parse it
+    Line example: username \t board_name:1,2,3,5 \t ... \n
+    :param line:
+    :return:
+    """
+    pass
+
+if __name__ == '__main__':
+    pass
