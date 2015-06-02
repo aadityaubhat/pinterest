4efcdec6e9a70d9f9d41d9f7b0b358c0b78c18c0
AadityaBhat <aadityaubhat@gmail.com>
2015-06-01 22:23:48 -0400

    Error Handling

--- dataparsing.py
+++ dataparsing.py
@@ -10,26 +10,42 @@ import time
 def parse_link(link):
     #download xml file;
     #check if the file exists or else download;
-    if not (os.path.isfile('data_aaditya/mainlink.xml')):
-        file = urllib.URLopener()
-        file.retrieve(link, "data_aaditya/mainlink.xml")
-    root = xml.etree.ElementTree.parse("data_aaditya/mainlink.xml").getroot()
-    result = {}
-    for i in xrange(0,len(root)):
-        result[i] = root[i][0].text, root[i][1].text.rsplit('T')[0]
-    return result
+    while True:
+        try:
+            if not (os.path.isfile('data_aaditya/mainlink.xml')):
+                file = urllib.URLopener()
+                file.retrieve(link, "data_aaditya/mainlink.xml")
+            root = xml.etree.ElementTree.parse("data_aaditya/mainlink.xml").getroot()
+            result = {}
+            for i in xrange(0,len(root)):
+                result[i] = root[i][0].text, root[i][1].text.rsplit('T')[0]
+            return result
+        except IOError:
+            uri = "mongodb://aaditya:sangam@localhost:27017/pindb"
+            client = pymongo.MongoClient(uri)
+            doc =  {"URL" : link}
+            client['pindb']['error_log'].insert(doc)
+
 
 def parse_gz(lst):
-    if not (os.path.isfile(('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]))):
-        file = urllib.URLopener()
-        file.retrieve(lst[0], ('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]))
-    f = gzip.open(('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]), 'rb')
-    root = xml.etree.ElementTree.fromstring(f.read())
-    f.close()
-    result = {}
-    for i in xrange(0,len(root)):
-        result[i] = root[i][0].text, lst[1]
-    return result
+    while True:
+        try:
+            if not (os.path.isfile(('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]))):
+                file = urllib.URLopener()
+                file.retrieve(lst[0], ('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]))
+            f = gzip.open(('b:/pinterest/data_aaditya/'+lst[0].rsplit('/')[4]), 'rb')
+            root = xml.etree.ElementTree.fromstring(f.read())
+            f.close()
+            result = {}
+            for i in xrange(0,len(root)):
+                result[i] = root[i][0].text, lst[1]
+            return result
+        except IOError:
+            uri = "mongodb://aaditya:sangam@localhost:27017/pindb"
+            client = pymongo.MongoClient(uri)
+            doc =  {"GZ File" : lst[0].rsplit('/')[4] }
+            client['pindb']['error_log'].insert(doc)
+
 
 def slicelink(lst):
     return lst[0].rsplit('/')[3], lst[0].rsplit('/')[4], lst[1]
@@ -38,7 +54,8 @@ def main(fileurl,n=1):
     linklist = parse_link(fileurl)
     uri = "mongodb://aaditya:sangam@localhost:27017/pindb"
     client = pymongo.MongoClient(uri)
-    for i in xrange(366,n):
+    # test: remove 400 for actual program
+    for i in xrange(400,n):
         gzfiles = parse_gz(linklist[i])
         docs = []
         for j in xrange(0, len(gzfiles)):
