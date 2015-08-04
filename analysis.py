f79df472001bbfd0514373fdf06c0a113d22b1a2
AadityaBhat <aadityaubhat@gmail.com>
2015-08-04 03:24:57 -0400

    optimized hist, top 100 functions. New plot functions for hist and top 100

--- analysis.py
+++ analysis.py
@@ -1,11 +1,11 @@
 __author__ = 'aaditya'
 
-import gzip
-import re
-import pandas
-import bokeh
+import gzip,marshal
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
-pandas.DataFrame
 def read_data():
     """
     Read data from the data/data.gz
@@ -13,37 +13,48 @@ def read_data():
     #for test purpose, only a subset of users being considered
     :return: dictionary of board and users
     """
-    res = {}
+    data = defaultdict(list)
     with gzip.open('data/data.gz') as file:
-        for line in file:
+        for i,line in enumerate(file):
             #remove if loop to read all the users
-            if re.match('^ab[a-z]*[A-Z]*[0-9]*', line):
-                if line.split('\t')[1].replace('\n', '') in res:
-                    res[line.split('\t')[1].replace('\n', '')].append(line.split('\t')[0])
-                else:
-                    res[line.split('\t')[1].replace('\n', '')] = []
-                    res[line.split('\t')[1].replace('\n', '')].append(line.split('\t')[0])
-    return  res
+            if line.startswith('ab'):
+                username , boardname = line.strip().split('\t')
+                data[username].append(boardname)
+    return data
 
-def top_50_boards(board_user):
-    top50 = {}
-    for i, board in enumerate(board_user):
-        if i == 49:
-            break
-        else:
-            top50[i] = [board,len(board_user[board])]
-    for board in board_user:
-        for i in xrange(len(top50)):
-            if len(board_user[board]) > top50[i][1]:
-                top50[i] = [board,len(board_user[board])]
-                break
-    return top50
+def store():
+    data = read_data()
+    fh = gzip.open("processed.gz",'w')
+    for k,v in data.iteritems():
+        fh.write(k+'\t'+'\t'.join(v)+'\n')
+    fh.close()
 
-def hist(board_users):
-    hist = {}
-    for board in board_users:
-        hist[board] = len(board_users[board])
-    return hist
+def hist(data):
+    count = defaultdict(int)
+    for boards in data.itervalues():
+        for b in boards:
+            count[b] += 1
+    return count
+
+def plot_hist(data):
+    counts = hist(data)
+    plt.hist(np.asarray([math.log10(counts[i]) for i in counts]), bins = 100)
+
+def top_100_boards(data):
+    counts = hist(data)
+    top = sorted([(v,k) for k,v in counts.iteritems()],reverse=True)[:100]
+
+def plot_top_100(data):
+    top = top_100_boards(data)
+    plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+    plt.tick_params(axis='both', which='major', labelsize=8)
+    plt.bar(np.arange(len([i[1] for i in top])*6, step = 6),[i[0] for i in top], width = 6)
+    plt.title('Top  100 boards')
+    plt.xticks(np.arange(len([i[1] for i in top])*6,step = 6), [i[1] for i in top], rotation= 'vertical', ha = 'left')
+    plt.show()
+
+
+
 
 if __name__ == '__main__':
     pass
